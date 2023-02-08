4cc11f545a0ebe2200a88f08aea24652f14e0aaa
xueyuan.nick <xueyuan.nick@foxmail.com>
2023-02-08 11:53:46 +0800

    添加 vite 代理配置

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        proxy: {
+            // 带选项写法：http://localhost:5173/api/bar -> http://jsonplaceholder.typicode.com/bar
+            '/api': {
+                target: 'http://jsonplaceholder.typicode.com',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+            // 代理 websockets 或 socket.io 写法：ws://localhost:5173/socket.io -> ws://localhost:5174/socket.io
+            '/websockets': {
+                target: 'ws://localhost:5174',
+                ws: true,
+            },
+        },
+    },
 })
